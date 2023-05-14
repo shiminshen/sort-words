49b41654404a658aaedde63c784c2574071cda28
Damon Shen <damon.s@binance.com>
2023-05-14 18:26:04 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { GameList } from "@/components/GameList";
 import { normalizeQuestions } from '@/utilities/cmsNormalizer';
 import { client } from "@/utilities/contentful";
 
+// force revalidate every 0 second
+export const revalidate = 0;
+
 export default async function Home() {
   const entries = await client.getEntries();
   const data = normalizeQuestions(entries.items)
